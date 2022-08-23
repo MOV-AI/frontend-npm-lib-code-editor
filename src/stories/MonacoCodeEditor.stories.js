38c2aeea2d78279e444cbf3dc6e7c023c1aa4e5a
PedroCristovao-Movai <pedro.cristovao@mov.ai>
2022-08-23 09:22:31 +0000

    update storybook

--- src/stories/MonacoCodeEditor.stories.js
+++ src/stories/MonacoCodeEditor.stories.js
@@ -2,6 +2,7 @@ import { withAuthentication } from "@mov-ai/mov-fe-lib-react";
 import React from "react";
 import withMock from "storybook-addon-mock";
 import MonacoCodeEditor from "../components/MonacoCodeEditor/MonacoCodeEditor.js";
+import { token } from "./tokens";
 
 export default {
   title: "Monaco Code Editor",
@@ -35,13 +36,7 @@ Python.parameters = {
       status: 200,
       response: (_) => {
         // MUST BE A REAL TOKEN FROM MOVAI BE
-        return {
-          refresh_token:
-            "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJSZWZyZXNoIiwiaXNzIjoiYmFja2VuZCIsImlhdCI6MTY2MTE4ODA1OCwiZXhwIjoxNjYxNzkyODU4LCJqdGkiOiI5MmRiN2QwOS0xMzdiLTQ5ZmItOTc4ZS1iMjg0YTI0YmM2MGUiLCJyZWZyZXNoX2lkIjoiIiwiZG9tYWluX25hbWUiOiJpbnRlcm5hbCIsImFjY291bnRfbmFtZSI6InBlZHJvIiwiY29tbW9uX25hbWUiOiJQZWRybyIsInVzZXJfdHlwZSI6IklOVEVSTkFMIiwicm9sZXMiOlsiRGVmYXVsdFJvbGUiXSwiZW1haWwiOiIiLCJzdXBlcl91c2VyIjp0cnVlLCJyZWFkX29ubHkiOmZhbHNlLCJzZW5kX3JlcG9ydCI6ZmFsc2V9.FRNmMzIDIYsShV1M18jCchBZVoPWPkXBFYlafx0wz_g",
-          access_token:
-            "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJBY2Nlc3MiLCJpc3MiOiJiYWNrZW5kIiwiaWF0IjoxNjYxMTg4MDU4LCJleHAiOjE2NjExOTE2NTgsImp0aSI6IjkwMTQyNDcyLTM0YWEtNGE5MC05MTA3LWJiYTJiYjIxZmM3MCIsInJlZnJlc2hfaWQiOiI5MmRiN2QwOS0xMzdiLTQ5ZmItOTc4ZS1iMjg0YTI0YmM2MGUiLCJkb21haW5fbmFtZSI6ImludGVybmFsIiwiYWNjb3VudF9uYW1lIjoicGVkcm8iLCJjb21tb25fbmFtZSI6IlBlZHJvIiwidXNlcl90eXBlIjoiSU5URVJOQUwiLCJyb2xlcyI6WyJEZWZhdWx0Um9sZSJdLCJlbWFpbCI6IiIsInN1cGVyX3VzZXIiOnRydWUsInJlYWRfb25seSI6ZmFsc2UsInNlbmRfcmVwb3J0IjpmYWxzZX0.UnbEx35CrqwJQiP3-tFBLiPpyVl8grJ2AcwjpULVtOY",
-          error: false,
-        };
+        return token;
       },
     },
   ],
---
+++ src/stories/tokens.js
@@ -0,0 +1,5 @@
+export const token = {
+  refresh_token: "",
+  access_token: "",
+  error: false,
+};
