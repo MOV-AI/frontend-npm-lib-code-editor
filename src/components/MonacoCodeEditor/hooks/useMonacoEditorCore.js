9e110258d7ad4410ba37308881fa4b457c73b404
Paulo Andre Azevedo Quirino <paulo.quirino@mov.ai>
2024-03-13 11:46:40 +0000

    Upgrade dependencies

--- src/components/MonacoCodeEditor/hooks/useMonacoEditorCore.js
+++ src/components/MonacoCodeEditor/hooks/useMonacoEditorCore.js
@@ -21,18 +21,18 @@ import ReconnectingWebSocket from "reconnecting-websocket";
 self.MonacoEnvironment = {
   getWorkerUrl: function (_, label) {
     if (label === "json") {
-      return "./lib/@mov-ai/mov-fe-lib-code-editor/json-worker.js";
+      return "./lib/@mov-ai/mov-fe-lib-code-editor/dist/json-worker.js";
     }
     if (label === "css" || label === "scss" || label === "less") {
-      return "./lib/@mov-ai/mov-fe-lib-code-editor/css-worker.js";
+      return "./lib/@mov-ai/mov-fe-lib-code-editor/dist/css-worker.js";
     }
     if (label === "html" || label === "handlebars" || label === "razor") {
-      return "./lib/@mov-ai/mov-fe-lib-code-editor/html.worker.js";
+      return "./lib/@mov-ai/mov-fe-lib-code-editor/dist/html.worker.js";
     }
     if (label === "typescript" || label === "javascript") {
-      return "./lib/@mov-ai/mov-fe-lib-code-editor/ts-worker.js";
+      return "./lib/@mov-ai/mov-fe-lib-code-editor/dist/ts-worker.js";
     }
-    return "./lib/@mov-ai/mov-fe-lib-code-editor/editor-worker.js";
+    return "./lib/@mov-ai/mov-fe-lib-code-editor/dist/editor-worker.js";
   },
 };
 
